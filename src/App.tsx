ba21003c669ce3863e1964daf9b20de836bf923b
Samer Kayali <samerkkr@gmail.com>
2022-11-21 08:21:27 -0300

    refactor: removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import ComponentUseReducerSolution from "./state-management-solutions/useReducerSolution";
 
 function App() {
